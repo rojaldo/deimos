1d7ac9d19558950ad8ecbcc457246bc7ab37eeac
Daniel Rojo Pérez <rojaldo@yahoo.com>
2019-07-08 13:46:33 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,10 +19,10 @@ describe('AppComponent', () => {
   it(`should have as title 'deimos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('deimos');
+    expect(app.title).toEqual({algo: 'uno'});
   });
 
-  it('should render title in a h1 tag', () => {
+  xit('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'deimos';
+  title = {algo: 'uno'};
+  myvar = 'una nueva variable';
 }
