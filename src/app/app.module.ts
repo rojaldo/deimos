a69135298dc426d6c419d044f737a029338dbb30
Daniel Rojo Pérez <rojaldo@yahoo.com>
2019-07-10 10:44:42 +0200

    apod component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroesFormComponent } from './components/heroes-form/heroes-form.compon
 import { HeroesService } from './services/heroes.service';
 import { BeersComponent } from './components/beers/beers.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ApodComponent } from './components/apod/apod.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +24,8 @@ import { HttpClientModule } from '@angular/common/http';
     HeroesComponent,
     HeroesListComponent,
     HeroesFormComponent,
-    BeersComponent
+    BeersComponent,
+    ApodComponent
   ],
   imports: [
     BrowserModule, NgbModule, FormsModule, HttpClientModule
---
+++ src/app/components/apod/apod.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from 'src/app/services/request.service';
+
+@Component({
+  selector: 'app-apod',
+  templateUrl: './apod.component.html',
+  styleUrls: ['./apod.component.scss']
+})
+export class ApodComponent implements OnInit {
+
+  result: any;
+  responded = false;
+
+  constructor(private service: RequestService) { }
+
+  ngOnInit() {
+    const apiKey = 'DEMO_KEY';
+    const url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey + '&date=2019-07-09';
+    this.service.getRequest(url).subscribe((data) => this.processData(data));
+  }
+
+  processData(data: any) {
+    this.result = data;
+    this.responded = true;
+  }
+
+}
