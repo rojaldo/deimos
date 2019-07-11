db102cd8ac14391c16d4834314c1daefc82b036e
Daniel Rojo Pérez <rojaldo@yahoo.com>
2019-07-11 09:41:02 +0200

    alcohol pipe with one parameter

--- src/app/pipes/alcohol.pipe.ts
+++ src/app/pipes/alcohol.pipe.ts
@@ -9,8 +9,23 @@ export class AlcoholPipe implements PipeTransform {
     if (typeof value === 'number') {
       const intPart = Math.floor(value);
       const decPart = Math.floor((value % 1) * 10);
+      let separator = ',';
+      if (args) {
+        switch (args[0]) {
+          case ',':
+            separator = ',';
+            break;
+          case '.':
+            separator = '.';
+            break;
 
-      return intPart + ',' + decPart + '%';
+          default:
+            separator = ',';
+            console.error('Wrong separator: ' + args[0]);
+            break;
+        }
+      }
+      return intPart + separator + decPart + '%';
     } else {
       console.error('Wrong type for alcohol pipe: it must be a number');
       return value;
